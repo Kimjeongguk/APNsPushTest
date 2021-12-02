69d7a15a49ff760292aa90cd2e44697ff69a101d
Kimjeongguk <mindstar7949@naver.com>
2021-12-02 09:23:20 +0900

    서버에서 메시지보내는거는 툴사용해서 보냄

--- APNSPushTest/AppDelegate.swift
+++ APNSPushTest/AppDelegate.swift
@@ -14,6 +14,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+//        registerForRemoteNotifications()
+        UNUserNotificationCenter.current().delegate = self
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { [weak self] granted, error in
+            print("Permission granted: \(granted)")
+            
+        }
+
+        application.registerForRemoteNotifications()
+
         return true
     }
 
@@ -31,6 +40,46 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
+//MARK: APNS푸시관련
+    private func registerForRemoteNotifications(){
+        let center = UNUserNotificationCenter.current()
+        center.delegate = self //UNUserNotificationCenterDelegate필요함
+        let options: UNAuthorizationOptions = [.alert, .sound, .badge]
+        center.requestAuthorization(options: options) { granted, error in
+            guard granted else {
+                return
+            }
+            
+            DispatchQueue.main.async {
+                //APNs에 디바이스 토큰 등록
+                UIApplication.shared.registerForRemoteNotifications()
+                
+            }
+            
+        }
+    }
+}
 
+
+extension AppDelegate: UNUserNotificationCenterDelegate {
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.alert, .badge, .sound])
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        let url = response.notification.request.content.userInfo
+        print("url = \(url)")
+    }
+    
+    // 실패시
+    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+        print("Failed to register for notifications: \(error.localizedDescription)")
+        
+    } // 성공시
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        let tokenParts = deviceToken.map { data in String(format: "%02.2hhx", data) }
+        let token = tokenParts.joined()
+        print("Device Token: \(token)")
+    }
 }
 
